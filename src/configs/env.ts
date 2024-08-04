08eb0824bed74a9cdae824266308b5fb0b472232
AnanthuTD <ananthutd2021@gmail.com>
2024-08-04 20:22:17 +0530

    feat: add MONGO_URI validation to environment configuration

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,10 +1,11 @@
-import { cleanEnv, str, port } from 'envalid';
+import { cleanEnv, str, port, url } from 'envalid';
 
 const env = cleanEnv(process.env, {
   NODE_ENV: str({
     choices: ['development', 'test', 'production', 'staging'],
   }),
   PORT: port(),
+  MONGO_URI: url(),
 });
 
 export default env;
