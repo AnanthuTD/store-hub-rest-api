871d0a0a2efdca092e2bc73836897d898bb02637
AnanthuTD <ananthutd2021@gmail.com>
2024-08-04 16:53:34 +0530

    Update ESLint configuration imports and plugin usage
    - Renamed `eslint-plugin-prettier` to `eslintConfigPrettier` for consistency with other imports.
    - Updated import styles for consistency.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,12 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import prettier from "eslint-config-prettier";
-import eslintPluginPrettier from "eslint-plugin-prettier";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.node } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  eslintPluginPrettier,
-  prettier,
+   { files: ['**/*.{js,mjs,cjs,ts}'] },
+   { languageOptions: { globals: globals.node } },
+   pluginJs.configs.recommended,
+   ...tseslint.configs.recommended,
+   eslintConfigPrettier,
 ];
