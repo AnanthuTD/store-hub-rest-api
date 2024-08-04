399dc67336c346051628126e82f181ec0397a939
AnanthuTD <ananthutd2021@gmail.com>
2024-08-04 20:22:49 +0530

    refactor: add MongoDB connection setup and improve server startup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import app from './app';
+import connectDB from './configs/db';
 import env from './configs/env';
 import logger from './utils/logger';
 
-// Start the server
-app.listen(env.PORT, () => {
-  logger.info(`Server is running on http://localhost:${env.PORT}`);
-});
+const start = async () => {
+  try {
+    connectDB();
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(env.PORT, () => {
+    logger.info(`Server is running on http://localhost:${env.PORT}`);
+  });
+};
+
+start();
