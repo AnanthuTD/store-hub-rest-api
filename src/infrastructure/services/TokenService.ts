4c40550a2fff7f510c742927bbd6dca97b37bc9d
AnanthuTD <ananthutd2021@gmail.com>
2024-08-07 12:33:05 +0530

    feat: Add TokenService for JWT token generation
    - Implemented `generateToken` method to create JWTs with user ID
    - Token expires in 1 hour and uses secret key from environment variables

---
+++ src/infrastructure/services/TokenService.ts
@@ -0,0 +1,12 @@
+import jwt from 'jsonwebtoken';
+import env from '../env/env';
+
+export class TokenService {
+  public static generateToken(userId: string): string {
+    return jwt.sign({ id: userId }, env.JWT_SECRET!, {
+      expiresIn: '1h',
+    });
+  }
+}
+
+export default TokenService;
