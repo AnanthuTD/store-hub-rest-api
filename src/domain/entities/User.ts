0a29c6d2437a7d8c2111839d66a1a72252e770e9
AnanthuTD <ananthutd2021@gmail.com>
2024-08-07 12:22:24 +0530

    feat: extend IUser interface with profile, account, and verificationToken fields

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -1,7 +1,38 @@
 export interface IUser {
+  id: string;
+  username?: string;
   email: string;
   password: string;
-  googleId?: string;
   phoneNumber?: string;
-  displayName?: string;
+  emailVerified?: boolean;
+
+  profile?: {
+    firstName: string;
+    lastName: string;
+    avatar?: string;
+    dateOfBirth?: Date;
+  };
+  createdAt?: Date;
+  lastLogin?: Date;
+  role?: string;
+  status?: string;
+
+  account?: {
+    type: string;
+    provider: string;
+    providerAccountId: string;
+    refreshToken?: string;
+    accessToken?: string;
+    expiresAt?: Date;
+    tokenType?: string;
+    scope?: string;
+    idToken?: string;
+    sessionState?: string;
+  }[];
+
+  verificationToken?: {
+    token: string;
+    expires: Date;
+    identified: string;
+  };
 }
