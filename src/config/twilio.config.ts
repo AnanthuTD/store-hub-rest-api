e327a62a6cdf580accfbc2f5ae3ba13f50a32e98
AnanthuTD <ananthutd2021@gmail.com>
2024-08-07 23:29:06 +0530

    feat: integrate Twilio for OTP sending and verification
    - Implemented `sendOTP` function to send an OTP via SMS to a given phone number.
    - Implemented `verifyOTP` function to verify an OTP against a given phone number.

---
+++ src/config/twilio.config.ts
@@ -0,0 +1,22 @@
+import { Twilio } from 'twilio';
+import env from '../infrastructure/env/env';
+
+const accountSid = env.TWILIO_ACCOUNT_SID;
+const authToken = env.TWILIO_AUTH_TOKEN;
+const serviceSid = env.TWILIO_SERVICE_SID;
+
+const twilioClient = new Twilio(accountSid, authToken);
+
+export const sendOTP = (phoneNumber: string) => {
+  return twilioClient.verify.v2.services(serviceSid).verifications.create({
+    to: phoneNumber,
+    channel: 'sms',
+  });
+};
+
+export const verifyOTP = (phoneNumber: string, code: string) => {
+  return twilioClient.verify.v2.services(serviceSid).verificationChecks.create({
+    to: phoneNumber,
+    code,
+  });
+};
