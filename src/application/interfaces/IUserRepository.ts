4609520c3a909b52454302904b4296c9bb936e6c
AnanthuTD <ananthutd2021@gmail.com>
2024-08-07 12:24:11 +0530

    feat: define IUserRepository interface with methods for user management

---
+++ src/application/interfaces/IUserRepository.ts
@@ -0,0 +1,8 @@
+import { IUser } from '../../domain/entities/User';
+
+export interface IUserRepository {
+  getUserByGoogleId(googleId: string): Promise<IUser | null>;
+  getUserByEmail(email: string): Promise<IUser | null>;
+  createUser(user: IUser): Promise<IUser>;
+  getUserById(id: string): Promise<IUser | null>;
+}
