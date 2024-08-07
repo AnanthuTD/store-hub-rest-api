ccf3441c903e52b0bfa76db266b7a628578ceeb5
AnanthuTD <ananthutd2021@gmail.com>
2024-08-07 12:30:41 +0530

    feat: add new environment variables for JWT and Google OAuth

--- src/infrastructure/env/env.ts
+++ src/infrastructure/env/env.ts
@@ -6,6 +6,10 @@ const env = cleanEnv(process.env, {
   }),
   PORT: port(),
   MONGO_URI: url(),
+  JWT_SECRET: str(),
+  JWT_EXPIRATION_TIME: str(),
+  GOOGLE_CLIENT_ID: str(),
+  GOOGLE_CLIENT_SECRET: str(),
 });
 
 export default env;
