e2d47416fd5b3201148b79cc169d4af8de9c2de8
AnanthuTD <ananthutd2021@gmail.com>
2024-08-07 12:26:14 +0530

    feat: implement Passport JWT strategy for token-based authentication

---
+++ src/infrastructure/auth/JwtStrategy.ts
@@ -0,0 +1,30 @@
+import passport from 'passport';
+import {
+  Strategy as JwtStrategy,
+  ExtractJwt,
+  StrategyOptionsWithoutRequest,
+} from 'passport-jwt';
+import UserRepository from '../repositories/UserRepository';
+import env from '../env/env';
+
+const userRepository = new UserRepository();
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: env.JWT_SECRET,
+} satisfies StrategyOptionsWithoutRequest;
+
+passport.use(
+  new JwtStrategy(opts, async (jwt_payload, done) => {
+    try {
+      const user = await userRepository.getUserById(jwt_payload.id);
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    } catch (err) {
+      return done(err, false);
+    }
+  })
+);
