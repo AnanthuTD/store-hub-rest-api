7ccbe8308e4026233599ad0a9cf96003286c06e0
AnanthuTD <ananthutd2021@gmail.com>
2024-08-07 23:57:48 +0530

    feat: DI for OTPController

--- src/infrastructure/dependencyInjection.ts
+++ src/infrastructure/dependencyInjection.ts
@@ -1,5 +1,7 @@
 import { IRegisterUser } from '../application/interfaces/IRegisterUser';
+import { OTPService } from '../application/usecases/OTPService';
 import RegisterUser from '../application/usecases/RegisterUser';
+import OTPController from '../interfaces/controllers/OTPController';
 import RegisterController from '../interfaces/controllers/RegisterController';
 import UserRepository from './repositories/UserRepository';
 
@@ -9,5 +11,8 @@ const userRepository = new UserRepository();
 // Controllers
 const registerUser: IRegisterUser = new RegisterUser(userRepository);
 const registerController = new RegisterController(registerUser);
+
+const otpService = new OTPService();
+export const otpController = new OTPController(otpService);
 
 export { registerController };
