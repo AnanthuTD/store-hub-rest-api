52399b63278d44af49994901849f69d165384799
AnanthuTD <ananthutd2021@gmail.com>
2024-08-15 00:23:41 +0530

    feat(auth): Implement credential-based authentication controller
    - Added `CredentialAuthController` to handle authentication using user credentials.
    - Integrated `TokenService` to generate JWT tokens and store them in HTTP-only cookies for secure client-side storage.
    - Configured the `authToken` cookie with secure attributes, including `httpOnly`, `secure`, `maxAge`, and `sameSite` to enhance security.
    - Implemented robust error handling to log issues with token generation and return a 500 status in case of failures.
    - Leveraged environment-based configuration (`env.isProduction`) to dynamically set the `secure` attribute for cookies.

--- src/interfaces/controllers/CredentialAuthController.ts
+++ src/interfaces/controllers/CredentialAuthController.ts
@@ -1,13 +1,22 @@
 import { Request, Response } from 'express';
 import { TokenService } from '../../infrastructure/services/TokenService';
-import logger from '../../infrastructure/utils/logger';
+import logger from '../../infrastructure/utils/Logger';
+import env from '../../infrastructure/env/env';
 
 class CredentialAuthController {
   async handle(req: Request, res: Response) {
     try {
-      const { id } = req.body;
-      const token = TokenService.generateToken(id);
-      res.json({ token });
+      const { user } = req;
+      const token = TokenService.generateToken(user.id);
+
+      res.cookie('authToken', token, {
+        httpOnly: true,
+        secure: env.isProduction,
+        maxAge: 24 * 60 * 60 * 1000,
+        sameSite: 'strict',
+      });
+
+      res.json({ user });
     } catch (error) {
       if (error instanceof Error) {
         logger.error('Error generating token:', error.message);
