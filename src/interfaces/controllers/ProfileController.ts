683b0f2eb400f0df39f2ace95e20aaf6e2afcec2
AnanthuTD <ananthutd2021@gmail.com>
2024-08-15 00:51:14 +0530

    fix(profile): Correct profile response structure in ProfileController
    - Updated `ProfileController` to return a structured response with `id` and `profile` fields.

--- src/interfaces/controllers/ProfileController.ts
+++ src/interfaces/controllers/ProfileController.ts
@@ -4,7 +4,7 @@ import { IUser } from '../../domain/entities/User';
 class ProfileController {
   async handle(req: Request, res: Response) {
     const user = req.user as IUser;
-    res.json(user);
+    res.json({ id: user.id, profile: user?.profile });
   }
 }
 
