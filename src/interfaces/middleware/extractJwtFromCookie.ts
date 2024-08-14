aa165cd2fef3b101641eb662657049bd64439159
AnanthuTD <ananthutd2021@gmail.com>
2024-08-14 21:05:16 +0530

    feat: implement middleware to extract JWT from cookies

---
+++ src/interfaces/middleware/extractJwtFromCookie.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+import logger from '../../infrastructure/utils/Logger';
+
+const extractJwtFromCookie = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token = req.cookies['authToken'];
+
+    if (token) {
+      req.headers['authorization'] = `Bearer ${token}`;
+    }
+  } catch (error) {
+    logger.error('Error extracting JWT from cookie:', error);
+  }
+
+  next();
+};
+
+export default extractJwtFromCookie;
