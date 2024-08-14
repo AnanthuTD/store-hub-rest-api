b0949cdc8aed7f060077b8bf72eafd11034fd69d
AnanthuTD <ananthutd2021@gmail.com>
2024-08-14 21:13:46 +0530

    feat: extend environment variable validation with additional fields
    - Added `FRONTEND_BASE_URL`, `FRONTEND_VERIFICATION_ROUTE`, `EMAIL_VERIFICATION_TOKEN_EXPIRATION_TIME`, `EMAIL_USER`, and `EMAIL_PASS`

--- src/infrastructure/env/env.ts
+++ src/infrastructure/env/env.ts
@@ -1,4 +1,4 @@
-import { cleanEnv, str, port, url } from 'envalid';
+import { cleanEnv, str, port, url, email } from 'envalid';
 
 const env = cleanEnv(process.env, {
   NODE_ENV: str({
@@ -13,6 +13,11 @@ const env = cleanEnv(process.env, {
   TWILIO_ACCOUNT_SID: str(),
   TWILIO_AUTH_TOKEN: str(),
   TWILIO_SERVICE_SID: str(),
+  FRONTEND_BASE_URL: url(),
+  FRONTEND_VERIFICATION_ROUTE: str(),
+  EMAIL_VERIFICATION_TOKEN_EXPIRATION_TIME: str(),
+  EMAIL_USER: email(),
+  EMAIL_PASS: str(),
 });
 
 export default env;
