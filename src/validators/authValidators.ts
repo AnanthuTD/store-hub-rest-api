f73f0038d729cb137f51e2ea3f394097fd9f77c6
AnanthuTD <ananthutd2021@gmail.com>
2024-08-15 00:55:57 +0530

    refactor(validators): Consolidate and optimize Zod validation schemas

--- src/validators/authValidators.ts
+++ src/validators/authValidators.ts
@@ -1,15 +1,27 @@
 import { z } from 'zod';
 
+// Schema for registering a new user
 export const registerSchema = z.object({
+  firstName: z
+    .string()
+    .min(4, { message: 'First Name should be at least 4 characters' }),
+  lastName: z
+    .string()
+    .min(1, { message: 'Last Name should be at least 1 character' }),
+  password: z
+    .string()
+    .min(6, { message: 'Password must be at least 6 characters long' }),
+});
+
+// Schema for logging in an existing user
+export const loginSchema = z.object({
   email: z.string().email({ message: 'Invalid email address' }),
   password: z
     .string()
     .min(6, { message: 'Password must be at least 6 characters long' }),
 });
 
-export const loginSchema = z.object({
+// Schema for verifying an email address
+export const emailVerificationSchema = z.object({
   email: z.string().email({ message: 'Invalid email address' }),
-  password: z
-    .string()
-    .min(6, { message: 'Password must be at least 6 characters long' }),
 });
