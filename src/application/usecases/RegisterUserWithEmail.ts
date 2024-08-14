643a97fb2cef61c19091d88b743fcf3b9bec1504
AnanthuTD <ananthutd2021@gmail.com>
2024-08-14 16:19:49 +0530

    chore: rename file for better clarity.

---
+++ src/application/usecases/RegisterUserWithEmail.ts
@@ -0,0 +1,19 @@
+import bcrypt from 'bcryptjs';
+import { IUser } from '../../domain/entities/User';
+import { IUserRepository } from '../interfaces/IUserRepository';
+
+class RegisterUser {
+  private userRepository: IUserRepository;
+
+  constructor(userRepository: IUserRepository) {
+    this.userRepository = userRepository;
+  }
+
+  async execute(email: string, password: string): Promise<IUser> {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser: IUser = { email, password: hashedPassword };
+    return await this.userRepository.createUser(newUser);
+  }
+}
+
+export default RegisterUser;
