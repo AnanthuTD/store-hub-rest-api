898c5044c0493a617e6695bfc4ae38d02ff119ae
AnanthuTD <ananthutd2021@gmail.com>
2024-08-14 21:04:44 +0530

    fix: removed email sanitization

--- src/interfaces/middleware/sanitization.ts
+++ src/interfaces/middleware/sanitization.ts
@@ -1,10 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
-import { escape, normalizeEmail } from 'validator';
+import { escape } from 'validator';
 
 export function sanitizeInput(req: Request, res: Response, next: NextFunction) {
   console.log(req.body);
   if (req.body) {
-    req.body.email = normalizeEmail(req.body.email);
     req.body.password = escape(req.body.password);
   }
   next();
