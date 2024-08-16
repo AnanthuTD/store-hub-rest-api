26e03ce1117a4763efc3167a6ff1a609154ae78c
AnanthuTD <ananthutd2021@gmail.com>
2024-08-17 00:23:25 +0530

    feat: Enhance authentication routes with additional security and OTP flows
    - Added routes for token verification and email verification to enhance security.
    - Integrated OTP-based registration and login, including a dedicated controller for OTP login.
    - Implemented country code fetching via a new route for better user experience.
    - Updated Google authentication route to disable session-based authentication.
    - Ensured all routes use secure sessionless authentication with JWT.
    - Applied input sanitization middleware to critical routes for improved security.

--- src/interfaces/routes/AuthRoutes.ts
+++ src/interfaces/routes/AuthRoutes.ts
@@ -3,11 +3,16 @@ import passport from 'passport';
 import { sanitizeInput } from '../middleware/sanitization';
 import {
   otpController,
+  otpLoginController,
+  otpRegisterController,
   registerController,
+  tokenVerificationController,
 } from '../../infrastructure/dependencyInjection';
 import ProfileController from '../controllers/ProfileController';
 import GoogleAuthController from '../controllers/GoogleAuthController';
 import CredentialAuthController from '../controllers/CredentialAuthController';
+import { fetchCountryCodes } from '../controllers/CountryCodeController';
+import EmailVerificationController from '../controllers/EmailVerificationController';
 
 const router = Router();
 
@@ -15,20 +20,37 @@ router.get('/', (req, res) => {
   res.json({ hi: 'auth' });
 });
 
+router.get('/country-codes', fetchCountryCodes);
+
+router.post(
+  '/verify-token',
+  tokenVerificationController.verifyToken.bind(tokenVerificationController)
+);
+
+router.post('/verify-email', EmailVerificationController.sendVerificationEmail);
+
 router.post(
   '/register',
   sanitizeInput,
   registerController.handle.bind(registerController)
 );
 
+router.post(
+  '/register-otp',
+  otpRegisterController.register.bind(otpRegisterController)
+);
+
 router.get(
   '/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] })
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+    session: false,
+  })
 );
 
 router.get(
   '/google/callback',
-  passport.authenticate('google', { failureRedirect: '/' }),
+  passport.authenticate('google', { failureRedirect: '/', session: false }),
   GoogleAuthController.handle
 );
 
@@ -39,13 +61,18 @@ router.get(
 );
 
 router.post(
-  '/login/credentials',
-  sanitizeInput,
-  passport.authenticate('local'),
+  '/sign-in/credentials',
+  passport.authenticate('local', { session: false }),
   CredentialAuthController.handle
 );
 
 router.post('/otp/send', otpController.sendOTP.bind(otpController));
+
+router.post(
+  '/signin/otp',
+  otpLoginController.loginWithOTP.bind(otpLoginController)
+);
+
 router.post('/otp/verify', otpController.verifyOTP.bind(otpController));
 
 export default router;
