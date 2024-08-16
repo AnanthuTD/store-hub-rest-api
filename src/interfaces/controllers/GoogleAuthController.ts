268a262b8e7656c13f95939842b9b262e5b9589e
AnanthuTD <ananthutd2021@gmail.com>
2024-08-17 00:19:53 +0530

    feat: Enhance Google OAuth controller to set auth token in HTTP-only cookie
    - Modified the Google authentication handler to set the authentication token in an HTTP-only cookie.
    - Updated token generation and response handling to improve security and user experience.
    - Added logging for better error tracking.

--- src/interfaces/controllers/GoogleAuthController.ts
+++ src/interfaces/controllers/GoogleAuthController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { IUser } from '../../domain/entities/User';
 import TokenService from '../../infrastructure/services/TokenService';
+import logger from '../../infrastructure/utils/Logger';
+import env from '../../infrastructure/env/env';
 
 class GoogleAuthController {
   async handle(req: Request, res: Response): Promise<void> {
@@ -10,11 +12,22 @@ class GoogleAuthController {
         res.status(401).json({ error: 'User not authenticated' });
         return;
       }
+
+      // Generate a token
       const token = TokenService.generateToken(user.id);
 
-      res.redirect(`/profile?token=${token}`);
+      // Set token in an HTTP-only cookie
+      res.cookie('authToken', token, {
+        httpOnly: true,
+        secure: env.isProduction,
+        maxAge: 24 * 60 * 60 * 1000, // 1 day
+        sameSite: 'strict',
+      });
+
+      // Redirect user to the profile page
+      res.redirect('http://localhost:5173/home');
     } catch (error) {
-      console.error('Error handling Google authentication:', error);
+      logger.error('Error handling Google authentication:', error);
       res.status(500).json({ error: 'Internal server error' });
     }
   }
