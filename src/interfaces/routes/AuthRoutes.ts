6aac380c35ac7209595c64eb2abfdb5c8db53960
AnanthuTD <ananthutd2021@gmail.com>
2024-08-17 18:53:21 +0530

    refactor(router): reorganize routes for improved structure and clarity
    - Switched from dependency injection to direct instantiation for controller instances in routes.

--- src/interfaces/routes/AuthRoutes.ts
+++ src/interfaces/routes/AuthRoutes.ts
@@ -1,45 +1,44 @@
 import { Router } from 'express';
 import passport from 'passport';
 import { sanitizeInput } from '../middleware/sanitization';
-import {
-  otpController,
-  otpLoginController,
-  otpRegisterController,
-  registerController,
-  tokenVerificationController,
-} from '../../infrastructure/dependencyInjection';
 import ProfileController from '../controllers/ProfileController';
 import GoogleAuthController from '../controllers/GoogleAuthController';
 import CredentialAuthController from '../controllers/CredentialAuthController';
 import { fetchCountryCodes } from '../controllers/CountryCodeController';
 import EmailVerificationController from '../controllers/EmailVerificationController';
+import RegisterWithEmailController from '../controllers/RegisterWithEmailController';
+import RegisterUserMobileController from '../controllers/RegisterWithMobileController';
+import TokenVerificationController from '../controllers/TokenVerificationController';
+import OTPController from '../controllers/OTPController';
+import SigninMobileController from '../controllers/SigninMobileController';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.json({ hi: 'auth' });
-});
-
+// get all country codes
 router.get('/country-codes', fetchCountryCodes);
 
-router.post(
-  '/verify-token',
-  tokenVerificationController.verifyToken.bind(tokenVerificationController)
+// verify token send by email
+router.post('/verify-token', (req, res) =>
+  new TokenVerificationController().verifyToken(req, res)
 );
 
-router.post('/verify-email', EmailVerificationController.sendVerificationEmail);
-
+// send verification email to the user before signup
 router.post(
-  '/register',
-  sanitizeInput,
-  registerController.handle.bind(registerController)
+  '/register/verify/email',
+  EmailVerificationController.sendVerificationEmail
 );
 
-router.post(
-  '/register-otp',
-  otpRegisterController.register.bind(otpRegisterController)
+// register user with email, password, firstName, lastName
+router.post('/register/email', sanitizeInput, (req, res) =>
+  new RegisterWithEmailController().handle(req, res)
 );
 
+// register user with mobile number, password, firstName, lastName, countryCode
+router.post('/register/mobile', (req, res) =>
+  new RegisterUserMobileController().handle(req, res)
+);
+
+// sign-in with google
 router.get(
   '/google',
   passport.authenticate('google', {
@@ -48,31 +47,33 @@ router.get(
   })
 );
 
+// sign-in with google callback
 router.get(
   '/google/callback',
   passport.authenticate('google', { failureRedirect: '/', session: false }),
   GoogleAuthController.handle
 );
 
+// get profile information of signed-in user
 router.get(
   '/profile',
   passport.authenticate('jwt', { session: false }),
   ProfileController.handle
 );
 
+// sign-in with local credentials (email and password)
 router.post(
-  '/sign-in/credentials',
+  '/signin/credentials',
   passport.authenticate('local', { session: false }),
   CredentialAuthController.handle
 );
 
-router.post('/otp/send', otpController.sendOTP.bind(otpController));
+// send OTP to user's registered mobile number
+router.post('/otp/send', (req, res) => new OTPController().sendOTP(req, res));
 
-router.post(
-  '/signin/otp',
-  otpLoginController.loginWithOTP.bind(otpLoginController)
+// sign-in with mobile number, otp and country code
+router.post('/signin/mobile', (req, res) =>
+  new SigninMobileController().handle(req, res)
 );
-
-router.post('/otp/verify', otpController.verifyOTP.bind(otpController));
 
 export default router;
