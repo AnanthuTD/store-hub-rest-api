708496c5bab80823e88a89b46c97077302d6af60
AnanthuTD <ananthutd2021@gmail.com>
2024-08-17 16:56:24 +0530

    chore: moved from application layer to interface layer.

--- src/infrastructure/services/OTPService.ts
+++ src/infrastructure/services/OTPService.ts
@@ -1,12 +1,19 @@
+import { VerificationCheckInstance } from 'twilio/lib/rest/verify/v2/service/verificationCheck';
 import { sendOTP, verifyOTP } from '../../config/twilio.config';
+import IOTPService from '../../domain/services/IOTPService';
+import { injectable } from 'inversify';
 
-export class OTPService {
+@injectable()
+export class OTPService implements IOTPService {
   async sendOTP(phoneNumber: string) {
     const verification = await sendOTP(phoneNumber);
     return verification.status;
   }
 
-  async verifyOTP(phoneNumber: string, code: string) {
+  async verifyOTP(
+    phoneNumber: string,
+    code: string
+  ): Promise<VerificationCheckInstance> {
     const verificationCheck = await verifyOTP(phoneNumber, code);
     if (verificationCheck.status !== 'approved') {
       throw new Error('Invalid OTP');
