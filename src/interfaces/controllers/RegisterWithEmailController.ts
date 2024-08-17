3e29a812235b5080d23481d6815d8d7e5b6d2c1b
AnanthuTD <ananthutd2021@gmail.com>
2024-08-17 15:50:59 +0530

    feat: implement RegisterWithEmailController for user registration
    - Added RegisterWithEmailController to handle registration via email.
    - Integrated Zod for input validation with detailed error handling.
    - Utilized RegisterUserUseCase for executing the registration logic.
    - Configured dependency injection using inversify to fetch RegisterUserUseCase.
    - Implemented comprehensive error handling to manage validation errors and internal server issues.
    
    This controller provides a robust endpoint for user registration, ensuring validated input and proper error handling.

---
+++ src/interfaces/controllers/RegisterWithEmailController.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from 'express';
+import { registerSchema } from '../../validators/authValidators';
+import { ZodError } from 'zod';
+import RegisterUserUseCase from '../../application/usecases/RegisterUserWithEmail';
+import { container } from '../../config/inversify.config';
+import { TYPES } from '../../config/types';
+
+class RegisterWithEmailController {
+  private registerUserUseCase = container.get<RegisterUserUseCase>(
+    TYPES.RegisterUserUseCase
+  );
+
+  async handle(req: Request, res: Response): Promise<void> {
+    const { firstName, lastName, password, token } = req.body;
+
+    try {
+      // Validate input using Zod
+      try {
+        registerSchema.parse({ password, firstName, lastName });
+      } catch (error) {
+        if (error instanceof ZodError) {
+          res
+            .status(400)
+            .json({ errors: error.errors.map((error) => error.message) });
+          return;
+        }
+        throw error;
+      }
+
+      // Execute the use case
+      const user = await this.registerUserUseCase.execute({
+        password,
+        firstName,
+        lastName,
+        token,
+      });
+
+      // Respond with the newly created user
+      res.status(201).json({ user });
+    } catch (error) {
+      if (error instanceof Error && error.message) {
+        res.status(400).json({ message: error.message });
+      } else {
+        res.status(500).json({ message: 'Internal server error' });
+      }
+      console.error('Error during registration:', error);
+    }
+  }
+}
+
+export default RegisterWithEmailController;
