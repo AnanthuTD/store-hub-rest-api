f3737661aded04747fcb8e76a9055438bf14b5a5
AnanthuTD <ananthutd2021@gmail.com>
2024-08-17 15:37:36 +0530

    chore: moved to domain layer

--- src/domain/repositories/IUserRepository.ts
+++ src/domain/repositories/IUserRepository.ts
@@ -1,4 +1,4 @@
-import { IUser } from '../../domain/entities/User';
+import { IUser } from '../entities/User';
 
 export interface IUserRepository {
   getUserByGoogleId(googleId: string): Promise<IUser | null>;
--- src/domain/repositories/IVerificationTokenRepository.ts
+++ src/domain/repositories/IVerificationTokenRepository.ts
@@ -2,6 +2,6 @@ import { IToken } from '../../domain/entities/Token';
 
 export interface IVerificationTokenRepository {
   createToken(tokenData: IToken): Promise<null>;
-  findToken(token: string): Promise<IToken>;
+  findToken(token: string): Promise<IToken | null>;
   removeToken(token: string): Promise<null>;
 }
