75f023aa9e63f5afe4fb77937a16de6f121e8882
AnanthuTD <ananthutd2021@gmail.com>
2024-08-17 15:42:21 +0530

    refactor: enhance user registration process with dependency injection and Clean Architecture
    - Refactored RegisterUser to RegisterUserUseCase using dependency injection with inversify.
    - Added support for token verification using ITokenVerifier.
    - Moved password hashing to a dedicated IPasswordHasher service.
    - Improved code structure by separating concerns and aligning with Clean Architecture principles.
    - Removed tokens after successful registration via IVerificationTokenRepository.
    
    These changes increase testability, maintainability, and flexibility of the user registration process.

--- src/application/usecases/RegisterUserWithEmail.ts
+++ src/application/usecases/RegisterUserWithEmail.ts
@@ -1,35 +1,54 @@
-import bcrypt from 'bcryptjs';
+import { inject, injectable } from 'inversify';
 import { IUser } from '../../domain/entities/User';
-import { IUserRepository } from '../interfaces/IUserRepository';
+import { IUserRepository } from '../../domain/repositories/IUserRepository';
+import IPasswordHasher from '../../domain/services/IPasswordHasher';
+import { TYPES } from '../../config/types';
+import { ITokenVerifier } from '../../domain/services/ITokenVerifier';
+import { IVerificationTokenRepository } from '../../domain/repositories/IVerificationTokenRepository';
 
 interface RegisterUserExecuteProps {
-  email: string;
   password: string;
   firstName: string;
   lastName: string;
+  token: string;
 }
 
-class RegisterUser {
-  private userRepository: IUserRepository;
-
-  constructor(userRepository: IUserRepository) {
-    this.userRepository = userRepository;
-  }
+@injectable()
+class RegisterUserUseCase {
+  public constructor(
+    @inject(TYPES.UserRepository)
+    private readonly userRepository: IUserRepository,
+    @inject(TYPES.PasswordHasher)
+    private readonly passwordHasher: IPasswordHasher,
+    @inject(TYPES.TokenVerifier)
+    private readonly tokenVerifier: ITokenVerifier,
+    @inject(TYPES.VerificationTokenRepository)
+    private readonly verificationTokenRepository: IVerificationTokenRepository
+  ) {}
 
   async execute({
-    email,
     password,
     firstName,
     lastName,
+    token,
   }: RegisterUserExecuteProps): Promise<IUser> {
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const { valid, message, email } =
+      await this.tokenVerifier.verifyToken(token);
+    if (!valid) {
+      throw new Error(message);
+    }
+
+    const hashedPassword = await this.passwordHasher.hashPassword(password);
     const newUser: IUser = {
       email,
       password: hashedPassword,
       profile: { firstName, lastName },
     };
+
+    this.verificationTokenRepository.removeToken(token);
+
     return await this.userRepository.createUser(newUser);
   }
 }
 
-export default RegisterUser;
+export default RegisterUserUseCase;
