1fdfa31449182fc2cf13511676c350ef3f751204
AnanthuTD <ananthutd2021@gmail.com>
2024-08-17 16:26:13 +0530

    feat: configure Inversify container for dependency injection
    - Set up Inversify container for managing dependency injection across the application.
    - Bound interfaces to their respective implementations:
      - IPasswordHasher to BcryptPasswordHasher
      - IUserRepository to UserRepository
      - RegisterUserUseCase to RegisterUserUseCase
      - ITokenVerifier to TokenVerifier
      - IVerificationTokenRepository to VerificationTokenRepository
      - SendVerificationEmailUseCase to SendVerificationEmailUseCase
      - IEmailService to EmailService
    
    This configuration provides a centralized mechanism for dependency management, ensuring that services and use cases are correctly instantiated and injected.

---
+++ src/config/inversify.config.ts
@@ -0,0 +1,31 @@
+import { Container } from 'inversify';
+import { TYPES } from './types';
+import IPasswordHasher from '../domain/services/IPasswordHasher';
+import BcryptPasswordHasher from '../infrastructure/services/PasswordHasher';
+import { IUserRepository } from '../domain/repositories/IUserRepository';
+import UserRepository from '../infrastructure/repositories/UserRepository';
+import RegisterUserUseCase from '../application/usecases/RegisterUserWithEmail';
+import { ITokenVerifier } from '../domain/services/ITokenVerifier';
+import TokenVerifier from '../infrastructure/services/TokenVerifier';
+import { IVerificationTokenRepository } from '../domain/repositories/IVerificationTokenRepository';
+import VerificationTokenRepository from '../infrastructure/repositories/VerificationTokenRepository';
+import SendVerificationEmailUseCase from '../application/usecases/SendVerificationEmailUseCase';
+import IEmailService from '../domain/services/IEmailService';
+import EmailService from '../infrastructure/services/EmailService';
+
+const container = new Container();
+container.bind<IPasswordHasher>(TYPES.PasswordHasher).to(BcryptPasswordHasher);
+container.bind<IUserRepository>(TYPES.UserRepository).to(UserRepository);
+container
+  .bind<RegisterUserUseCase>(TYPES.RegisterUserUseCase)
+  .to(RegisterUserUseCase);
+container.bind<ITokenVerifier>(TYPES.TokenVerifier).to(TokenVerifier);
+container
+  .bind<IVerificationTokenRepository>(TYPES.VerificationTokenRepository)
+  .to(VerificationTokenRepository);
+container
+  .bind<SendVerificationEmailUseCase>(TYPES.SendVerificationEmailUseCase)
+  .to(SendVerificationEmailUseCase);
+container.bind<IEmailService>(TYPES.EmailService).to(EmailService);
+
+export { container };
