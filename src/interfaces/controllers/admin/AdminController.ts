18825f2d2ec003c241d04ee7245fbb7caa4b151b
AnanthuTD <ananthutd2021@gmail.com>
2024-08-31 19:51:03 +0530

    feat(controller): admin signin.
    - return jwt token
    - store jwt in cookies

--- src/interfaces/controllers/admin/AdminController.ts
+++ src/interfaces/controllers/admin/AdminController.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import { container } from '../../../config/inversify.config';
 import { ISignInAdminUseCase } from '../../../application/usecases/SignInAdminUseCase';
 import { TYPES } from '../../../config/types';
+import env from '../../../infrastructure/env/env';
 
 export const signInAdmin = async (req: Request, res: Response) => {
   const { email, password } = req.body;
@@ -12,6 +13,14 @@ export const signInAdmin = async (req: Request, res: Response) => {
       TYPES.ISignInAdminUseCase
     );
     const token = await signInUseCase.execute(email, password);
+
+    res.cookie('authToken', token, {
+      httpOnly: true,
+      secure: env.isProduction,
+      maxAge: 24 * 60 * 60 * 1000,
+      sameSite: 'strict',
+    });
+
     res.json({ token });
   } catch (error) {
     res.status(400).json({ error: (error as Error).message });
