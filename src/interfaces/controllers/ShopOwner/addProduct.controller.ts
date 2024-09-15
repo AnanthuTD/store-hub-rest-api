c5fae01b0a910812ccd74c0a5b9bc2226058d8ef
AnanthuTD <ananthutd2021@gmail.com>
2024-09-15 15:36:31 +0530

    feat: include extra fields

--- src/interfaces/controllers/ShopOwner/addProduct.controller.ts
+++ src/interfaces/controllers/ShopOwner/addProduct.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import Products from '../../../infrastructure/database/models/ProductsSchema';
 import StoreProducts from '../../../infrastructure/database/models/StoreProducts';
+import Category from '../../../infrastructure/database/models/CategoryModel';
 
 // Add product by vendor
 export const addProductByVendor = async (req: Request, res: Response) => {
@@ -28,34 +29,44 @@ export const addProductByVendor = async (req: Request, res: Response) => {
   }
 
   try {
-    // Step 1: Check if the product already exists in the centralized collection
-    let product = await Products.findOne({ name: name, brand: brand });
+    // Step 1: Check if the category exists
+    const categoryDoc = await Category.findOne({ _id: category });
 
-    // Step 2: If the product does not exist, add it to the centralized collection
+    if (!categoryDoc) {
+      return res.status(400).json({ message: 'Category does not exist' });
+    }
+
+    // Step 2: Check if the product already exists in the centralized collection
+    let product = await Products.findOne({ name, brand });
+
+    // Step 3: If the product does not exist, add it to the centralized collection
     if (!product) {
       product = new Products({
-        name: name,
-        category: category,
-        brand: brand,
+        name,
+        category: { name: categoryDoc.name, _id: categoryDoc._id }, // Store category details
+        brand,
         brandId: null, // Set BrandId accordingly if you manage brands elsewhere
-        description: description,
+        description,
       });
       await product.save();
     }
 
-    // Step 3: Add the product to the store-specific collection
+    // Step 4: Add the product to the store-specific collection
     const storeProduct = new StoreProducts({
-      storeId: storeId,
-      sku: sku,
-      stock: stock,
+      storeId,
+      sku,
+      stock,
       productId: product._id, // Link to the centralized product
-      price: price,
-      images: images,
-      description: description,
+      name: product.name,
+      category: { name: categoryDoc.name, _id: categoryDoc._id }, // Store category details
+      brand,
+      price,
+      images,
+      description,
       attributes: JSON.parse(attributes),
       specifications: JSON.parse(specifications),
       variants: JSON.parse(variants),
-      status: status,
+      status,
       createdAt: new Date(),
       updatedAt: new Date(),
       metadata: { purchases: 0, views: 0 },
