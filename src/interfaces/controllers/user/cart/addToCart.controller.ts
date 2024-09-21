143bbc262d44416161145f9d96dad47fd593b25b
AnanthuTD <ananthutd2021@gmail.com>
2024-09-21 15:13:45 +0530

    feat: addToCart

---
+++ src/interfaces/controllers/user/cart/addToCart.controller.ts
@@ -0,0 +1,70 @@
+import { Request, Response } from 'express';
+import Cart from '../../../../infrastructure/database/models/CartSchema';
+import Products from '../../../../infrastructure/database/models/ProductsSchema';
+import logger from '../../../../infrastructure/utils/logger';
+
+interface AddToCartBody {
+  productId: string;
+  variantId: string;
+  quantity: number;
+}
+
+export const addToCart = async (req: Request, res: Response) => {
+  const { productId, variantId, quantity }: AddToCartBody = req.body;
+  const userId = req.user._id as string;
+
+  if (!productId || !variantId || quantity === undefined || quantity <= 0) {
+    return res.status(400).json({ error: 'Invalid input data.' });
+  }
+
+  try {
+    // Check if the product and variant exist
+    const product = await Products.findOne({
+      _id: productId,
+      'variants._id': variantId,
+    });
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product or variant not found.' });
+    }
+
+    // Find or create the cart for the user
+    const cart = await Cart.findOne({ userId });
+
+    if (!cart) {
+      // Create a new cart if it doesn't exist
+      const newCart = new Cart({
+        userId,
+        products: [{ productId, variantId, quantity }],
+      });
+
+      await newCart.save();
+      return res
+        .status(201)
+        .json({ message: 'Product added to new cart.', cart: newCart });
+    } else {
+      // Check if the product variant already exists in the cart
+      const productIndex = cart.products.findIndex(
+        (p) =>
+          p.productId.toString() === productId &&
+          p.variantId.toString() === variantId
+      );
+
+      if (productIndex > -1) {
+        // If product already exists in cart, update the quantity
+        cart.products[productIndex].quantity! += quantity;
+      } else {
+        // Add new product variant to the cart
+        cart.products.push({ productId, variantId, quantity });
+      }
+
+      await cart.save();
+      return res
+        .status(200)
+        .json({ message: 'Product added/updated in cart.', cart });
+    }
+  } catch (error) {
+    logger.error('Error adding product to cart:', error);
+    return res.status(500).json({ error: 'Failed to add product to cart.' });
+  }
+};
