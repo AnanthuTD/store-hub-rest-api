8e903d560832ca109be9af0be564cb6a9f351a5d
AnanthuTD <ananthutd2021@gmail.com>
2024-09-21 15:15:43 +0530

    feat: export all cart controllers from a single file

---
+++ src/interfaces/controllers/user/cart/index.ts
@@ -0,0 +1,13 @@
+import { checkItemInCart } from './checkItemInCart.controller';
+import { decrementProductInCart } from './decrementProductInCart.controller';
+import { getCartItems } from './getCartItems.controller';
+import { removeProductFromCart } from './removeProductFromCart.controller';
+import { addToCart } from './addToCart.controller';
+
+export {
+  checkItemInCart,
+  removeProductFromCart,
+  decrementProductInCart,
+  getCartItems,
+  addToCart,
+};
