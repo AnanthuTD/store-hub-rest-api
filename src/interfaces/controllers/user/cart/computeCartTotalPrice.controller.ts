2cebd33a9e0eeb4ca17953a895a107e2c66cee02
AnanthuTD <ananthutd2021@gmail.com>
2024-09-27 15:01:20 +0530

    feat: route to get the total price of the entire cart

---
+++ src/interfaces/controllers/user/cart/computeCartTotalPrice.controller.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from 'express';
+import Cart from '../../../../infrastructure/database/models/CartSchema';
+import StoreProducts from '../../../../infrastructure/database/models/StoreProducts';
+import logger from '../../../../infrastructure/utils/logger';
+
+// Controller to calculate the total price of all items in the user's cart
+export const calculateTotalPrice = async (req: Request, res: Response) => {
+  const userId = req.user?._id as string; // assuming userId is available in the req object
+
+  try {
+    const cart = await Cart.findOne({ userId });
+
+    if (!cart || cart.products.length === 0) {
+      return res.status(404).json({ message: 'Cart is empty or not found.' });
+    }
+
+    let totalPrice = 0;
+    let itemsEligibleToBuy = 0;
+
+    // Loop through each product in the cart
+    for (const cartProduct of cart.products) {
+      // Fetch the product and its variant to get the price
+      const product = await StoreProducts.findOne({
+        _id: cartProduct.productId,
+        'variants._id': cartProduct.variantId,
+      }).select('variants');
+
+      if (!product) {
+        return res.status(404).json({ message: 'Product not found.' });
+      }
+
+      const variant = product.variants.find(
+        (variant) => variant._id.toString() === cartProduct.variantId.toString()
+      );
+
+      if (!variant) {
+        return res.status(404).json({ message: 'Variant not found.' });
+      }
+
+      if (variant.stock >= cartProduct.quantity) {
+        // Calculate total price by multiplying quantity with variant price
+        const itemTotal =
+          (variant.discountedPrice || variant.price) * cartProduct.quantity;
+        totalPrice += itemTotal.toFixed(2);
+        itemsEligibleToBuy += 1;
+      }
+    }
+
+    return res.status(200).json({ totalPrice, itemCount: itemsEligibleToBuy });
+  } catch (error) {
+    logger.error('Error calculating total price:', { error, userId });
+    return res
+      .status(500)
+      .json({ message: 'Failed to calculate total price.' });
+  }
+};
--- src/interfaces/routes/user/cart.ts
+++ src/interfaces/routes/user/cart.ts
@@ -6,6 +6,7 @@ import {
   getCartItems,
   removeProductFromCart,
 } from '../../controllers/user/cart/index';
+import { calculateTotalPrice } from '../../controllers/user/cart/computeCartTotalPrice.controller';
 const router = express.Router();
 
 router.get('/', getCartItems);
@@ -13,5 +14,6 @@ router.get('/check', checkItemInCart);
 router.post('/add', addToCart);
 router.patch('/decrement', decrementProductInCart);
 router.delete('/remove', removeProductFromCart);
+router.get('/total', calculateTotalPrice);
 
 export default router;
