58f526e571645b0fb4c62fa85e373b6f145dc5ad
AnanthuTD <ananthutd2021@gmail.com>
2024-10-08 16:20:06 +0530

    test

--- src/interfaces/routes/deliveryPartner/index.ts
+++ src/interfaces/routes/deliveryPartner/index.ts
@@ -3,6 +3,8 @@ import authRouter from './auth';
 import protectedRoutes from './protected';
 import passport from 'passport';
 import locationRoutes from './locationRoutes';
+import deliveryRoutes from './deliveryRouter';
+import { assignDeliveryPartnerForOrder } from '../../../infrastructure/services/partnerAssignmentService';
 const partnerRouter = express.Router();
 
 partnerRouter.use('/auth', authRouter);
@@ -13,5 +15,20 @@ partnerRouter.use(
   passport.authenticate('partner-jwt', { session: false }),
   protectedRoutes
 );
+
+partnerRouter.use(
+  '/delivery',
+  passport.authenticate('partner-jwt', { session: false }),
+  deliveryRoutes
+);
+
+partnerRouter.get('/notify', function () {
+  assignDeliveryPartnerForOrder({
+    orderId: '66f6f2f973ee00f755f395e4',
+    storeLongitude: 76.353775,
+    storeLatitude: 9.996966,
+    retryCount: 0,
+  });
+});
 
 export default partnerRouter;
