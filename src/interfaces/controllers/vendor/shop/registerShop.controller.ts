e0efbdaa8db84330e6d95fc81b5e5963df11bcb7
AnanthuTD <ananthutd2021@gmail.com>
2024-11-01 07:27:03 +0530

    feat: add new store id to vendor schema

--- src/interfaces/controllers/vendor/shop/registerShop.controller.ts
+++ src/interfaces/controllers/vendor/shop/registerShop.controller.ts
@@ -4,6 +4,7 @@ import Shop, {
 } from '../../../../infrastructure/database/models/ShopSchema';
 import logger from '../../../../infrastructure/utils/logger';
 import { getRequestUserId } from '../../../../infrastructure/utils/authUtils';
+import ShopOwner from '../../../../infrastructure/database/models/ShopOwnerModel';
 
 export default async function registerShop(
   req: Request,
@@ -16,6 +17,10 @@ export default async function registerShop(
     data.ownerId = getRequestUserId(req);
     const shopData = await Shop.create({ ...data, isVerified: false });
     shopData.save();
+    await ShopOwner.updateOne(
+      { _id: shopData.ownerId },
+      { $push: { stores: shopData._id } }
+    );
     res.status(201).json({ message: 'Shop created successfully' });
   } catch (error) {
     res.status(500).json({ message: 'Failed to create shop' });
