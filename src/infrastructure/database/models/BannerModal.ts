bed32c41f66c99a9020f4448db89b00beed79679
AnanthuTD <ananthutd2021@gmail.com>
2024-11-02 20:12:16 +0530

    updated to include subtitle and button

--- src/infrastructure/database/models/BannerModal.ts
+++ src/infrastructure/database/models/BannerModal.ts
@@ -2,16 +2,20 @@ import mongoose, { Document, Schema } from 'mongoose';
 
 export interface IBanner extends Document {
   title: string;
+  subtitle: string;
   imageUrl?: string;
   link?: string;
+  buttonText?: string;
   startDate: Date;
   endDate: Date;
 }
 
 const bannerSchema: Schema = new Schema({
   title: { type: String, required: true },
+  subtitle: { type: String, required: true },
   imageUrl: String,
   link: String,
+  buttonText: String,
   startDate: { type: Date, required: true },
   endDate: { type: Date, required: true },
 });
